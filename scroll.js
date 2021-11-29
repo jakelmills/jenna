e5f1265065fb6fb167454f32634b975d0ddfc209
Jake Mills <jakelmills@protonmail.com>
2021-11-29 20:19:50 +0000

    added js for progress bar along top to update as user scrolls

--- scroll.js
+++ scroll.js
@@ -1,4 +1,6 @@
 const pixelsTag = document.querySelector("div.pixels")
+const bodyTag = document.querySelector("body")
+const progressTag = document.querySelector("div.progress")
 
 //when page is scrolled update the pixels tag to say how far scrolled
 
@@ -6,3 +8,15 @@ document.addEventListener('scroll', function(){
   const pixels = window.pageYOffset
   pixelsTag.innerHTML = Math.floor(pixels)
 })
+
+document.addEventListener('scroll',function(){
+  const pixels = window.pageYOffset
+  const pageHeight = bodyTag.getBoundingClientRect().height
+  const totalScrollDistance = pageHeight - window.innerHeight
+  const scrollPercent = pixels / totalScrollDistance
+  progressTag.style.width = `${100 * scrollPercent}%`
+  console.log(scrollPercent)
+
+})
+
+//when page is scrolled make progress bar update accordingly
