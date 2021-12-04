032bd5783bf6d9c83ca06af546219240239a3204
Jake Mills <jakelmills@protonmail.com>
2021-12-04 09:15:43 +0000

    added JS to change client name & page num when scroll onto relevent sections. added JS to change text & progress bar color to white when on dark section

--- scroll.js
+++ scroll.js
@@ -1,6 +1,10 @@
 const pixelsTag = document.querySelector("div.pixels")
 const bodyTag = document.querySelector("body")
+const headerTag = document.querySelector('header')
 const progressTag = document.querySelector("div.progress")
+const sections = document.querySelectorAll('section')
+const clientTag = document.querySelector('div.client')
+const pageTag = document.querySelector('div.page')
 
 //when page is scrolled update the pixels tag to say how far scrolled
 
@@ -8,6 +12,8 @@ document.addEventListener('scroll', function(){
   const pixels = window.pageYOffset
   pixelsTag.innerHTML = Math.floor(pixels)
 })
+
+//when page is scrolled make progress bar update accordingly
 
 document.addEventListener('scroll',function(){
   const pixels = window.pageYOffset
@@ -19,4 +25,23 @@ document.addEventListener('scroll',function(){
 
 })
 
-//when page is scrolled make progress bar update accordingly
+//when page is scrolled, see how far done and check for each section
+//if passed section update the text in header
+
+document.addEventListener('scroll', function() {
+  const pixels = window.pageYOffset
+  sections.forEach(section => {
+    if (section.offsetTop - 60 <= pixels) {
+      clientTag.innerHTML = section.getAttribute('data-client')
+      pageTag.innerHTML = section.getAttribute('data-page')
+
+      if (section.hasAttribute('data-isdark')) {
+        headerTag.classList.add('white')
+        progressTag.classList.add('white')
+      } else {
+        headerTag.classList.remove('white')
+        progressTag.classList.remove('white')
+      }
+    }
+  })
+})
